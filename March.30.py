e0156e1cce890f961763d15bd4649ea11498367f
Jiang-Yingchun <18850605389@163.com>
2026-03-30 19:21:24 +0800

    Add files via upload
    学习了输出的代码

---
+++ March.30.py
@@ -0,0 +1,64 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:01:40) [MSC v.1944 32 bit (Intel)] on win32
+Enter "help" below or click "Help" above for more information.
+x=3
+print(x)
+3
+love_mathmetic1314=666
+print(love_mathmetic1314)
+666
+瑞幸咖啡=9.9
+print(瑞幸咖啡)
+9.9
+name ="曲径通幽处，禅房花木深"
+print(name)
+曲径通幽处，禅房花木深
+name ="此曲只应天上有"
+print(name)
+此曲只应天上有
+x=3
+y=42
+y=x
+print(y)
+3
+x=4
+y=7
+x,y=y,x
+print(x.y)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    print(x.y)
+AttributeError: 'int' object has no attribute 'y'
+print(x,y)
+7 4
+print('I love mathmatic')
+I love mathmatic
+>>> print("I love English and my English teacher")
+I love English and my English teacher
+>>> print('"Life is short,you need Python"')
+"Life is short,you need Python"
+>>> print('\"Lifr is short,let\'s learn Python"\')
+...       
+SyntaxError: unterminated string literal (detected at line 1); perhaps you escaped the end quote?
+>>> print('\"Lifr is short,let\'s learn Python"\')
+...       
+SyntaxError: unterminated string literal (detected at line 1); perhaps you escaped the end quote?
+>>> print('\"Lifr is short,let\'s learn Python"\')
+...       
+SyntaxError: unterminated string literal (detected at line 1); perhaps you escaped the end quote?
+>>> print('\"Lifr is short,let\'s learn Python.\"')
+...       
+"Lifr is short,let's learn Python."
+>>> print('\"life is short,let\'s learn Python.\"')
+...       
+"life is short,let's learn Python."
+>>> print('\"life is short,let\' learn Python\"')
+...       
+"life is short,let' learn Python"
+>>> print("I love mathmetic.\nI love Python")
+...       
+I love mathmetic.
+I love Python
+>>> print("I love mathmetic.\nI love Python")
+...       
+I love mathmetic.
+I love Python
