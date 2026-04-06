446611c4666a9e1bdf5156e58d0de2470f5174cc
Jiang-Yingchun <18850605389@163.com>
2026-04-06 14:46:11 +0800

    Add files via upload
    good mood

---
+++ good_mood.py
@@ -0,0 +1,17 @@
+s="hello world!"
+print(len(s))
+r="林如欣666无敌了孩子"
+print(len(r))
+print(s[3])
+print(r[3])
+print(s[len(s)-1])
+print(r[len(r)-1])
+print(r[len(r)-3])
+b1=True
+b2=False
+n=None
+print(type(s))
+print(type(b1))
+print(type(n))
+print(type(b2))
+print(type(r))
