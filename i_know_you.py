10d922a68f7299f10fc99aab9a3695264a917bbd
Jiang-Yingchun <18850605389@163.com>
2026-04-15 08:25:30 +0800

    Add files via upload

---
+++ i_know_you.py
@@ -0,0 +1,9 @@
+class BigCat:
+    def __init__(self,cat_name,cat_age,cat_color):
+        self.name=cat_name
+        self.age=cat_age
+        self.color=cat_color
+cat1=BigCat(cat_name="樱花",cat_age=3,cat_color="白色")
+print(f"小猫名字:{cat1.name}小猫年龄:{cat1.age}小猫花色:{cat1.color}")
+
+
