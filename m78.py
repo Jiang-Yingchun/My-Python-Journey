1bb8df463ba99ac229938cca02a4ed03b97f92ea
Jiang-Yingchun <18850605389@163.com>
2026-05-05 08:10:23 +0800

    Add files via upload

---
+++ m78.py
@@ -0,0 +1,11 @@
+# 使用turtle库的turtle.fd0函数和turtle.seth0函数绘制一个等边三角形，边长为200像素，效果如下图所示。
+import turtle
+turtle.fd(200)
+turtle.seth(120)
+turtle.fd(200)
+turtle.seth(-120)
+turtle.fd(200)
+turtle.done()
+
+
+
