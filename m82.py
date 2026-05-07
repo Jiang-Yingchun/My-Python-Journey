1b372c0f3c3730abc6024c9eecfb70c36e49f650
Jiang-Yingchun <18850605389@163.com>
2026-05-07 08:53:45 +0800

    Add files via upload

---
+++ m82.py
@@ -0,0 +1,30 @@
+
+# 45、考生文件夹下存在一个文件PY202.py，该文件是本题目的代码提示框架，其中代码可以任意修改。请在该文件中编写代码，以实现如下功能：
+# 键盘输入小明学习的课程名称及考分等信息，信息间采用空格分隔，每个课程一行，空行回车结束录入，示例格式如下：
+# 数学 90
+# 语文95
+# 英语86
+# 物理84
+# 生物 87
+# 屏幕输出得分最高的课程及成绩，得分最低的课程及成绩，以及平均分（保留2位小数）。
+# 注意，其中逗号为英文逗号，格式如下：
+# 最高分课程是语文95，最低分课程是物理84，平均分是88.40
+data=[]
+while True:
+    s=input()
+    if not s.strip():
+        break
+    subject,grade=s.split()
+    data.append((subject,int(grade)))
+max_info=max(data,key=lambda x:x[1])
+min_info=min(data,key=lambda x:x[1])
+avg=sum(m[1] for m in data)/len(data)
+print(f"最高分课程是{max_info[0]}{max_info[1]},最低分课程是{min_info[0]}{min_info[1]},平均分是{avg:.2f}")
+
+
+
+
+
+
+
+
